4e4227078d0db6366b73639ef9d557e06d2221d3
nstazer <nstazer99@gmail.com>
2021-03-27 13:17:07 -0400

    Bottom Navigation Bar

--- app/src/main/java/com/example/lexicoolapp/MainActivity.java
+++ app/src/main/java/com/example/lexicoolapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.lexicoolapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        //Profile fragment
+                        break;
+                    case R.id.action_random:
+                        //Random Word fragment
+                        break;
+                    case R.id.action_search:
+                    default:
+                        //Search fragment
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_random);
     }
 }
