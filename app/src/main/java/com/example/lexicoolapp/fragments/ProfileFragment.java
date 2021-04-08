d1a8cad85b1d5da6b1f276b971ef7a686a5e4a17
Omar <omar.darras21@gmail.com>
2021-04-08 13:41:31 -0400

    basic profile

--- app/src/main/java/com/example/lexicoolapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/lexicoolapp/fragments/ProfileFragment.java
@@ -1,26 +1,30 @@
 package com.example.lexicoolapp.fragments;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.lexicoolapp.R;
 
-public class ProfileFragment extends AppCompatActivity {
+public class ProfileFragment extends Fragment {
 
     private ImageView ivProfile;
     private TextView tvUsername;
     private TextView tvCoins;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_fragment);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ivProfile = view.findViewById(R.id.ivProfile);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvCoins = view.findViewById(R.id.tvCoins);
 
-        ivProfile = findViewById(R.id.ivProfile);
-        tvUsername = findViewById(R.id.tvUsername);
-        tvCoins = findViewById(R.id.tvCoins);
     }
 }
