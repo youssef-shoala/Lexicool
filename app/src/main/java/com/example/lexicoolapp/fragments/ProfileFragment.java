d8ab545ae7c76ecdc9a1d7e27d85a9a836ec430a
Omar <omar.darras21@gmail.com>
2021-04-08 14:10:55 -0400

    ProfileFragment

--- app/src/main/java/com/example/lexicoolapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/lexicoolapp/fragments/ProfileFragment.java
@@ -2,12 +2,12 @@ package com.example.lexicoolapp.fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +18,17 @@ public class ProfileFragment extends Fragment {
     private ImageView ivProfile;
     private TextView tvUsername;
     private TextView tvCoins;
+
+    public ProfileFragment() {
+        // Required constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
